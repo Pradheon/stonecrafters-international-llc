ca25ed087aa9b6e4d81381a8372cc8744b679adc
Joshan Rai <60300252+Pradheon@users.noreply.github.com>
2023-11-14 21:30:15 -0800

    Add mobile responsiveness to all sections except navigation

--- js/color-scheme-toggle.js
+++ js/color-scheme-toggle.js
@@ -1,4 +1,7 @@
 function setPreferredColorScheme() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+
+    let yelpReview = document.querySelectorAll(".review.island.island-light");
+    yelpReview.classList.toggle("dark-mode-yelp")
 }
