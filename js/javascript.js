92cf413b39d580f0d76bb5f0d18aa334aa247af8
Joshan Rai <60300252+Pradheon@users.noreply.github.com>
2023-11-14 22:39:52 -0800

    Add mobile responsiveness styling, javascript, and html for navigation links

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,24 @@
+function toggleMenu() {
+  let navMenu = document.getElementById("nav-menu-wrapper");
+  let links = document.getElementById("nav-items");
+  let expand = document.getElementById("expand");
+  let collapse = document.getElementById("collapse");
+
+  if (links.style.display === "flex") {
+    navMenu.style.background = "rgba(0, 0, 0, 1.0)";
+    links.style.display = "none";
+    expand.style.display = "block";
+    collapse.style.display = "none";
+  } else {
+    links.style.display = "flex";
+    links.style.flexDirection = "column";
+    links.style.alignItems = "center";
+    links.style.gap = "32px";
+    expand.style.display = "none";
+    collapse.style.display = "block";
+  }
+}
+
 var $el, leftPos, newWidth;
 
 $("#nav-items").append("<li id='magic-line'></li>");
